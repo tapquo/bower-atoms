9c26540e527f86199508593902c5002b438aad84
pinaypunto <ina@tapquo.com>
2014-02-12 14:28:48 +0100

    0.2.12

---
+++ atoms.example.ide.js
@@ -0,0 +1,4 @@
+/* atoms v0.2.02 - 2014/1/13
+   http://atomic.tapquo.com
+   Copyright (c) 2014 Tapquo S.L. - Licensed  */
+(function(){this.Ide=function(){var init;return init=function(){return alert("Go!")},{init:init}}()}).call(this);
